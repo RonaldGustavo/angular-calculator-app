ed2e5206f3e884b4df75e9ff7c8b238c8906ca93
Ronald Gustavo <Ronald@dbo.id>
2025-09-12 10:50:26 +0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'calculator', pathMatch: 'full' },
+  { path: 'calculator', component: AppComponent },
+  { path: '**', redirectTo: 'calculator' }
+];
